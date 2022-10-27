43b182a5afefdcf9971a161dc77abe56f4b69749
Egor Badmaev <60363270+htmlprogrammist@users.noreply.github.com>
2022-10-27 20:18:45 +0300

    feat: create `Resources` module

---
+++ Resources/Resources-Swift.h
@@ -0,0 +1,18 @@
+//
+//  Resources.h
+//  Resources
+//
+//  Created by Егор Бадмаев on 27.10.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Models.
+FOUNDATION_EXPORT double ModelsVersionNumber;
+
+//! Project version string for Models.
+FOUNDATION_EXPORT const unsigned char ModelsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Models/PublicHeader.h>
+
+
