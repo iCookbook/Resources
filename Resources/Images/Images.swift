caf4a97ea79a55574d430ecfcff22ac6585237b3
Egor Badmaev <60363270+htmlprogrammist@users.noreply.github.com>
2022-10-27 20:19:12 +0300

    feat: add images

---
+++ Resources/Images/Images.swift
@@ -0,0 +1,18 @@
+//
+//  Images.swift
+//  Resources
+//
+//  Created by Егор Бадмаев on 27.10.2022.
+//
+
+import UIKit
+
+public enum Images {
+    static let frameworkBundle = Bundle(identifier: "com.egbad.Resources")
+    
+    public enum TabBarItems {
+        public static let discover = UIImage(named: "doc.text.image", in: frameworkBundle, compatibleWith: nil)
+        public static let search = UIImage(named: "magnifyingglass", in: frameworkBundle, compatibleWith: nil)
+        public static let person = UIImage(named: "person.fill", in: frameworkBundle, compatibleWith: nil)
+    }
+}
