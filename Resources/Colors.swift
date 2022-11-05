bf1b7b8ca1e70f63acaa0fc5dcb30de04c3069ed
Egor Badmaev <60363270+htmlprogrammist@users.noreply.github.com>
2022-11-06 00:42:54 +0300

    feat: made minor updates in `Colors` and `Images`
    * Changed colors to more correct ones (tested on iOS 15.5 on iPhone 13)
    * Unwrap `sampleRecipeImage`

--- Resources/Colors.swift
+++ Resources/Colors.swift
@@ -21,17 +21,17 @@ public enum Colors {
     
     public static let systemGroupedBackground: UIColor = {
         if #available(iOS 13.0, *) {
-            return UIColor.systemGroupedBackground
+            return UIColor.secondarySystemBackground
         } else {
-            return UIColor(red: 249/255, green: 249/255, blue: 249/255, alpha: 1.0)
+            return UIColor(red: 242/255, green: 242/255, blue: 247/255, alpha: 1.0)
         }
     }()
     
     public static let transparentTitleLabel: UIColor = {
         if #available(iOS 13.0, *) {
-            return UIColor.label.withAlphaComponent(0.8)
+            return UIColor.label.withAlphaComponent(0.9)
         } else {
-            return UIColor.black.withAlphaComponent(0.8)
+            return UIColor.black.withAlphaComponent(0.9)
         }
     }()
     
@@ -43,14 +43,7 @@ public enum Colors {
         }
     }()
     
-    public static let lightSecondaryLabel: UIColor = {
-        if #available(iOS 13.0, *) {
-            // TODO: Исправить это, чтобы выглядело нормально
-            return UIColor.secondaryLabel
-        } else {
-            return UIColor(red: 230/255, green: 230/255, blue: 230/255, alpha: 0.9)
-        }
-    }()
+    public static let lightSecondaryLabel = UIColor(red: 230/255, green: 230/255, blue: 230/255, alpha: 0.9)
     
     public static let tertiaryLabel: UIColor = {
         if #available(iOS 13.0, *) {
