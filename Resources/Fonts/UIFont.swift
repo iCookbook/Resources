78255c2ebe0f7d326a3b16a3543fa5dd454f271d
Egor Badmaev <60363270+htmlprogrammist@users.noreply.github.com>
2022-11-07 02:03:41 +0300

    feat: add fonts to the project

---
+++ Resources/Fonts/UIFont.swift
@@ -0,0 +1,35 @@
+//
+//  UIFont.swift
+//  Resources
+//
+//  Created by Егор Бадмаев on 06.11.2022.
+//
+
+import UIKit
+
+/// This code registers fonts for the project.
+extension UIFont {
+    public static func registerFont(bundle: Bundle, fontName: String, fontExtension: String) {
+        guard let path = bundle.path(forResource: "Resources", ofType: "bundle"),
+              let bundle = Bundle(path: path),
+              let fontURL = bundle.url(forResource: fontName, withExtension: fontExtension)
+        else {
+            fatalError("Couldn't find font \(fontName)")
+        }
+        
+        guard let fontDataProvider = CGDataProvider(url: fontURL as CFURL) else {
+            fatalError("Couldn't load data from the font \(fontName)")
+        }
+        
+        guard let font = CGFont(fontDataProvider) else {
+            fatalError("Couldn't create font from data")
+        }
+        
+        var error: Unmanaged<CFError>?
+        let success = CTFontManagerRegisterGraphicsFont(font, &error)
+        guard success else {
+            print("Error registering font: maybe it was already registered.")
+            return
+        }
+    }
+}
