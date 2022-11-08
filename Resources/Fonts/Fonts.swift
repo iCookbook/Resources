f0dbf3c313dca07f9ea403fcdd9f8b35ddbab8a8
Egor Badmaev <60363270+htmlprogrammist@users.noreply.github.com>
2022-11-08 22:03:30 +0300

    feat: add app's fonts

--- Resources/Fonts/Fonts.swift
+++ Resources/Fonts/Fonts.swift
@@ -10,7 +10,7 @@ import UIKit
 /// The use of fonts in the application is centralized and described here.
 ///
 /// We have serif font _"Ney York"_ and list of app's fonts.
-/// It was described as `final class` in order to find its' bundle.
+/// It was described as Сlass in order to find its' bundle. Keyword `final` is for static dispatch => better perfomance.
 public final class Fonts {
     
     // MARK: - Public Methods
@@ -22,24 +22,6 @@ public final class Fonts {
         UIFont.registerFont(bundle: Bundle(for: Fonts.self), fontName: "NewYorkLarge-Bold", fontExtension: "otf")
     }
     
-    // MARK: New York
-    
-    public static func newYorkRegular(ofSize: CGFloat) -> UIFont {
-        UIFont(name: "NewYorkLarge-Regular", size: ofSize) ?? .systemFont(ofSize: ofSize)
-    }
-    
-    public static func newYorkMedium(ofSize: CGFloat) -> UIFont {
-        UIFont(name: "NewYorkLarge-Medium", size: ofSize) ?? .systemFont(ofSize: ofSize, weight: .medium)
-    }
-    
-    public static func newYorkSemibold(ofSize: CGFloat) -> UIFont {
-        UIFont(name: "NewYorkLarge-Semibold", size: ofSize) ?? .systemFont(ofSize: ofSize, weight: .semibold)
-    }
-    
-    public static func newYorkBold(ofSize: CGFloat) -> UIFont {
-        UIFont(name: "NewYorkLarge-Bold", size: ofSize) ?? .systemFont(ofSize: ofSize, weight: .bold)
-    }
-    
     // MARK: App's fonts
     
     public static func title() -> UIFont {
@@ -48,5 +30,55 @@ public final class Fonts {
     
     public static func headline() -> UIFont {
         newYorkBold(ofSize: 24)
+    }
+    
+    public static func navControllerTitle() -> UIFont {
+        newYorkBold(ofSize: 18)
+    }
+    
+    public static func body() -> UIFont {
+        newYorkRegular(ofSize: 18)
+    }
+    
+    public static func medium() -> UIFont {
+        newYorkMedium(ofSize: 18)
+    }
+    
+    public static func semibold() -> UIFont {
+        newYorkSemibold(ofSize: 18)
+    }
+    
+    public static func smallBody() -> UIFont {
+        newYorkRegular(ofSize: 16)
+    }
+    
+    public static func smallMedium() -> UIFont {
+        newYorkMedium(ofSize: 16)
+    }
+    
+    public static func smallSemibold() -> UIFont {
+        newYorkSemibold(ofSize: 16)
+    }
+    
+    public static func subtitle() -> UIFont {
+        .systemFont(ofSize: 16, weight: .medium)
+    }
+    
+    // MARK: New York
+    
+    private static func newYorkRegular(ofSize: CGFloat) -> UIFont {
+        UIFont(name: "NewYorkLarge-Regular", size: ofSize) ?? .systemFont(ofSize: ofSize)
+    }
+    
+    private static func newYorkMedium(ofSize: CGFloat) -> UIFont {
+        UIFont(name: "NewYorkLarge-Medium", size: ofSize) ?? .systemFont(ofSize: ofSize, weight: .medium)
+    }
+    
+    private static func newYorkSemibold(ofSize: CGFloat) -> UIFont {
+        UIFont(name: "NewYorkLarge-Semibold", size: ofSize) ?? .systemFont(ofSize: ofSize, weight: .semibold)
+    }
+    
+    private static func newYorkBold(ofSize: CGFloat) -> UIFont {
+        UIFont(name: "NewYorkLarge-Bold", size: ofSize) ?? .systemFont(ofSize: ofSize, weight: .bold)
     }
 }
